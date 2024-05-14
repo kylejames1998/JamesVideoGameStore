b4dcfdfd7c997282bad3d2c0dac45d5105d5d6de
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-05-14 09:03:59 -0700

    added DbContext

---
+++ JamesVIdeoGameStore/Data/JamesVGSContext.cs
@@ -0,0 +1,18 @@
+﻿using JamesVIdeoGameStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JamesVIdeoGameStore.Data
+{
+    public class JamesVGSContext : DbContext
+    {
+        public JamesVGSContext(DbContextOptions<JamesVGSContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
