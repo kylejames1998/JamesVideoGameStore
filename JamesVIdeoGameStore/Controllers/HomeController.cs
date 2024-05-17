4323bfa67bc079ddcf2ff1fc94ed005bbc46f86c
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-05-17 08:31:25 -0700

    made changes to landing page

--- JamesVIdeoGameStore/Controllers/HomeController.cs
+++ JamesVIdeoGameStore/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace JamesVIdeoGameStore.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
--- JamesVIdeoGameStore/Models/Review.cs
+++ JamesVIdeoGameStore/Models/Review.cs
@@ -4,15 +4,13 @@ namespace JamesVIdeoGameStore.Models
     public class Review
     {
         [Key]
-        [StringLength(10)]
         public int ReviewId { get; set; }
 
         [Required(ErrorMessage = "Please enter a GameId.")]
-        [StringLength(10)]
         public int GameId { get; set; }
 
         [Required(ErrorMessage = "Please enter an OrderId.")]
-        [StringLength(10)]
+        
         public int OrderId { get; set; }
 
         [Required]
